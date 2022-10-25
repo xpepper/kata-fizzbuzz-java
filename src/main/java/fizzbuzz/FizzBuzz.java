4774770c403e976e2716d46064db8cc2cf4a780b
Pietro Di Bello <pierodibello@gmail.com>
2022-10-25 21:56:05 +0200

    remove redundant else

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -8,11 +8,11 @@ public class FizzBuzz {
 	public String translate(int number) {
 		if (((number % 5) == 0) && ((number % 7) == 0)) // A multiple of both?
 			return "FizzBuzz";
-		else if ((number % 5) == 0)
+		if ((number % 5) == 0)
 			return "Fizz"; // else a multiple of 5?
-		else if ((number % 7) == 0)
+		if ((number % 7) == 0)
 			return "Buzz"; // else a multiple of 7?
-		else
-			return String.valueOf(number); // else just print it
+
+		return String.valueOf(number); // else just print it
 	}
 }
