dcd3b46378e20ee774856f2845590985f0eaeee1
Matteo Pierro <pierromatteo@gmail.com>
2022-11-17 23:11:34 +0200

    refactor: ToStringRule implements Rule

--- src/main/java/fizzbuzz/ToStringRule.java
+++ src/main/java/fizzbuzz/ToStringRule.java
@@ -1,10 +1,20 @@
 package fizzbuzz;
 
-public class ToStringRule {
+public class ToStringRule implements Rule {
     public ToStringRule() {
     }
 
     public String evaluate(int number) {
         return String.valueOf(number);
+    }
+
+    @Override
+    public String apply(int number) {
+        return null;
+    }
+
+    @Override
+    public boolean appliesTo(int number) {
+        return false;
     }
 }
