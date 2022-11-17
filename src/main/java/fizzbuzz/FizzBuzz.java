f00a94457be17b323bcdcb43101ce24c9d282043
Pietro Di Bello <pierodibello@gmail.com>
2022-11-17 23:04:52 +0100

    refactor: extract field

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -7,11 +7,12 @@ public class FizzBuzz {
 
     private final Rule fizzRule = new FizzRule();
     private final Rule buzzRule = new BuzzRule();
-    private final Rule sumRule = new SumRule(fizzRule, buzzRule);
     private final Rule toStringRule = new ToStringRule();
+    private final Rule sumRule = new SumRule(fizzRule, buzzRule);
+    private final Rule orRule = new OrRule(sumRule, toStringRule);
 
     public String translate(int number) {
-        return new OrRule(sumRule, toStringRule).apply(number);
+        return orRule.apply(number);
     }
 
 }
