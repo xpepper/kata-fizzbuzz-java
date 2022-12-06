f672c4becd7a079e7591f2d8b8d6567c277af141
Joe Bew <joebew42@gmail.com>
2022-12-06 22:20:01 +0100

    feat: return Foo for multiple of 7

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -14,14 +14,26 @@ import fizzbuzz.rule.ToStringRule;
 public class FizzBuzz {
 
     private final Rule fooRule = new FooRule();
+    private final Rule fooForMultipleOfSeven = new FooForMultipleOfSeven();
     private final Rule fizzRule = new FizzRule();
     private final Rule buzzRule = new BuzzRule();
     private final Rule toStringRule = new ToStringRule();
-    private final Rule sumRule = new SumRule(fooRule, new SumRule(fizzRule, buzzRule));
+    private final Rule sumRule = new SumRule(new OrRule(fooRule, fooForMultipleOfSeven), new SumRule(fizzRule, buzzRule));
     private final Rule orRule = new OrRule(sumRule, toStringRule);
 
     public String translate(int number) {
         return orRule.apply(number);
     }
 
+    private class FooForMultipleOfSeven extends Rule {
+        @Override
+        public boolean appliesTo(int number) {
+            return number % 7 == 0;
+        }
+
+        @Override
+        protected String doApply(int number) {
+            return "Foo";
+        }
+    }
 }
